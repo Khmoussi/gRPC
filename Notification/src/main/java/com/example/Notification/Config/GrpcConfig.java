8a01764d8ecb93daa01a87c8b9702db96c82af89
khmoussi <khmoussiaouina@gmail.com>
2024-05-13 11:48:19 +0100

    first test Greeter_service in notification_service called by user_service

--- Notification/src/main/java/com/example/Notification/Config/GrpcConfig.java
+++ Notification/src/main/java/com/example/Notification/Config/GrpcConfig.java
@@ -2,6 +2,28 @@ package com.example.Notification.Config;
 
 import com.example.Notification.Services.GreeterService;
 import com.example.grpc.GreeterGrpc;
-@GRpcService
-public class GrpcConfig extends GreeterService {
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+import io.grpc.stub.annotations.GrpcGenerated;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.IOException;
+
+@Configuration
+public class GrpcConfig {
+    @Value("${grpc.server.port}")
+    private int grpcServerPort;
+
+    @Bean
+    public Server grpcServer(GreeterService greeterService) throws IOException {
+        Server server = ServerBuilder.forPort(grpcServerPort)
+                .addService(greeterService)
+                .build();
+        server.start();
+        System.out.println("Server started, listening on " + grpcServerPort);
+
+        return server;
+    }
 }
---
+++ User/src/main/java/com/example/User/Services/GreeterClient.java
@@ -0,0 +1,46 @@
+package com.example.User.Services;
+
+import com.example.grpc.GreeterGrpc;
+import com.example.grpc.HelloRequest;
+import com.example.grpc.HelloResponse;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+
+import java.util.concurrent.TimeUnit;
+
+public class GreeterClient {
+    private final ManagedChannel channel;
+    private final GreeterGrpc.GreeterBlockingStub blockingStub;
+
+    public GreeterClient(String host, int port) {
+        this(ManagedChannelBuilder.forAddress(host, port)
+                .usePlaintext()
+                .build());
+    }
+
+    GreeterClient(ManagedChannel channel) {
+        this.channel = channel;
+        blockingStub = GreeterGrpc.newBlockingStub(channel);
+    }
+
+    public void shutdown() throws InterruptedException {
+        channel.shutdown().awaitTermination(5, TimeUnit.SECONDS);
+    }
+
+    public void sayHello(String name) {
+        HelloRequest request = HelloRequest.newBuilder().setName(name).build();
+        HelloResponse response = blockingStub.sayHello(request);
+        System.out.println("Greeting: " + response.getMessage());
+    }
+
+    public static void main(String[] args) throws Exception {
+        GreeterClient client = new GreeterClient("localhost", 9090);
+        try {
+            String user = "UserName";
+            client.sayHello(user);
+        } finally {
+            client.shutdown();
+        }
+    }
+
+}
